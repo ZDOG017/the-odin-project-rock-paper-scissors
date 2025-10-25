36cfce1535a1c4b517877c13b75d9ab27f93c71b
Soltan Bolatov <iamkazakh02@gmail.com>
2025-10-25 16:24:45 +0300

    feat: add getComputerChoice function

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-console.log("Hello")
+function getComputerChoice () {
+    let rand = Math.floor(Math.random() * 3)
+    let result;
+    if (rand === 0) {
+        result = "Rock"
+    } else if (rand === 1) {
+        result = "Paper"
+    } else {
+        result = "Scissors"
+    }
+    console.log(result)
+    return result
+}
+
+getComputerChoice();
